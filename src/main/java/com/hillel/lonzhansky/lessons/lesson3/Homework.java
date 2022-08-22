c7ab502daea3e5b510a0f6b90affeb7a267d5f72
Lonzhansky <89276411+Lonzhansky@users.noreply.github.com>
2022-08-22 20:46:49 +0300

    change homework

--- src/main/java/com/hillel/lonzhansky/lessons/lesson3/Homework.java
+++ src/main/java/com/hillel/lonzhansky/lessons/lesson3/Homework.java
@@ -2,15 +2,14 @@ package com.hillel.lonzhansky.lessons.lesson3;
 
 public class Homework {
     public static void main(String[] args) {
+        
         double latitude = 34.5435454;
         String str = "Some place coords: " + latitude + '\u00B0';
         String str2 = "Some place coords: 40.234242 \\u00B0";
 
         System.out.println(str);
         System.out.println(str2);
-
-
+        
         double distance = latitude + 20;
-        double distance2 = latitude + 20;
     }
 }
